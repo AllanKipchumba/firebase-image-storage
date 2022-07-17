31bfa7e0ac47183809d85b81dddeb97258ff6aa0
_Allan <allankipchumba76@gmail.com>
2022-07-17 13:23:33 +0300

    get pictures from firebase functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { storage } from "./firebase";
-import { ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
 import { v4 } from "uuid";
+import "./app.css";
 
 const App = () => {
   const [imageUpload, setImageUpload] = useState(null);
+  const imageListRef = ref(storage, "profile-images/");
+  const [imageURL, setImageURL] = useState([]);
 
   const uploadImage = async () => {
     if (imageUpload == null) return;
@@ -15,18 +18,41 @@ const App = () => {
         storage,
         `profile-images/${imageUpload.name + v4()}`
       );
-      await uploadBytes(imageRef, imageUpload);
-      alert("uploaded");
+      uploadBytes(imageRef, imageUpload).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          setImageURL((prev) => [...prev, url]);
+        });
+      });
     } catch (error) {
       console.log(error);
     }
   };
 
+  // load the pictures each time it is uploaded
+  useEffect(() => {
+    try {
+      listAll(imageListRef).then((res) => {
+        console.log(res.items);
+        res.items.forEach((item) => {
+          getDownloadURL(item).then((url) => {
+            setImageURL((prev) => [...prev, url]);
+          });
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
-    <div>
+    <div className="App">
       <input type="file" onChange={(e) => setImageUpload(e.target.files[0])} />
 
       <button onClick={uploadImage}>Upload Image</button>
+
+      {imageURL.map((url) => {
+        return <img src={url} alt="/" className="img" />;
+      })}
     </div>
   );
 };
